9f05756658c10b77221a53c71da546ad65683e4d
novojitdas <novojitdas@gmail.com>
2021-08-14 13:33:30 +0600

    commit

---
+++ Array/00_ArrayInputOutputUpdate.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int student;
+    cout << "Enter the number of students:" << endl;
+    cin >> student;
+
+    int marks[10]; // it contains garbage values by default
+    //   another way to decalare array
+    //  int arr[10] = {0}; which contains all value as 0 by default
+
+    // input
+    for (int i = 0; i < student; i++)
+    {
+        cin >> marks[i];
+        // update
+        marks[i] = marks[i] * 2;
+    }
+
+    // output
+    for (int i = 0; i < student; i++)
+    {
+        cout << marks << ", ";
+    }
+
+    return 0;
+}
