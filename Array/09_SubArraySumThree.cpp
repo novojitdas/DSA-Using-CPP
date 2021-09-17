573e8d6ddc25be8e1c1ec9ed8027bfc305b42f34
novojitdas <novojitdas@gmail.com>
2021-09-17 23:51:24 +0600

    17/9

---
+++ Array/09_SubArraySumThree.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int kadaneSum(int arr[], int size)
+{
+    int current_sum = 0;
+    int largest_sum = 0;
+
+    for (int i = 0; i < size; i++)
+    {
+        current_sum = current_sum + arr[i];
+        if (current_sum < 0)
+        {
+            current_sum = 0;
+        }
+
+        largest_sum = max(current_sum, largest_sum);
+    }
+}
+
+int main()
+{
+    int arr[] = {-2, 3, 4, -1, 5, -12, 6, 1, 3};
+    int size = sizeoff(arr) / sizeoff(int);
+
+    return 0;
+}
